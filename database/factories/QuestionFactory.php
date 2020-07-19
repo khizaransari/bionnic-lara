ef899036df714acf70b4745d792cfbb853415c3d
khizar ansari <khizaansari721@gmail.com>
2020-07-20 04:13:29 +0500

    changs

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        'title' =>$faker->sentence(rand(5,10), '.') ,// rtrim($faker->sentence(rand(5,10)), '.'),
+        'title' =>rtrim($faker->sentence(rand(5,10)), '.') ,// rtrim($faker->sentence(rand(5,10)), '.'),
         'body' => $faker->paragraphs(rand(3,7), true),
         'views' => rand(0,10),
         // 'answers' => rand(0,10),
